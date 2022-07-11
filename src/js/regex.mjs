304582ebc1e39f04083f53901bf620f95a324b3c
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-07-11 14:42:58 +0300

    fix

---
+++ src/js/regex.mjs
@@ -0,0 +1,25 @@
+// #1
+// let quoteSample = '3 blind mice.';
+// let myRegex = /[^aeiou0-9]/gi; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
+// console.log(result);
+
+// #2
+// let difficultSpelling = 'Mississippi';
+// let myRegex = /s+/gi; // Change this line
+// let result = difficultSpelling.match(myRegex);
+// console.log(result);
+
+// #3
+// let chewieQuote = 'Aaaaaaaaaaaaaaaarrrgh!';
+// // Only change code below this line
+// let chewieRegex = /Aa*/; // Change this line
+// // Only change code above this line
+// let result = chewieQuote.match(chewieRegex);
+// console.log(result);
+
+// #4
+// let text = '<h1>Winter is coming</h1>';
+// let myRegex = /<.*?>/; // Change this line
+// let result = text.match(myRegex);
+// console.log(result);
